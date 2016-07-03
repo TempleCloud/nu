caa0dd4793e1caf5b53bd29e22b3fa5c7464abe2
TempleCloud <tim.langford@gmail.com>
2016-07-03 19:35:05 +0100

    Added another simple endpoint - 'echo'.

--- main.go
+++ main.go
@@ -6,9 +6,17 @@ import (
 
 func main() {
 	http.HandleFunc("/v1/ping", ping)
+	http.HandleFunc("/v1/echo", echo)
 	http.ListenAndServe(":9090", nil)
 }
 
+//--------------------------------------------------------------------------------------------------
+
 func ping(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("pong!"))
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	toEcho := r.URL.Query().Get("msg")
+	w.Write([]byte(toEcho))
 }
