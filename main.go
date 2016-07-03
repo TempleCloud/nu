f708606cfd397a548c2677a5c6dffb1dcc3d54c2
TempleCloud <tim.langford@gmail.com>
2016-07-03 19:52:37 +0100

    Added new docker image endpoint using the underlying '/var/run/docker.sock' socket with some rather dubious reads and writes.

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"log"
+	"net"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/v1/ping", ping)
 	http.HandleFunc("/v1/echo", echo)
+	http.HandleFunc("/v1/docker/images", dockerImages)
 	http.ListenAndServe(":9090", nil)
 }
 
@@ -16,7 +19,45 @@ func ping(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("pong!"))
 }
 
+//--------------------------------------------------------------------------------------------------
+
 func echo(w http.ResponseWriter, r *http.Request) {
 	toEcho := r.URL.Query().Get("msg")
 	w.Write([]byte(toEcho))
+}
+
+//--------------------------------------------------------------------------------------------------
+
+func dockerImages(w http.ResponseWriter, r *http.Request) {
+
+	log.Printf("Invoking!")
+
+	log.Printf("Creating socket!")
+	dockerSock, err := net.Dial("unix", "/var/run/docker.sock")
+	if err != nil {
+		panic(err)
+	}
+	defer dockerSock.Close()
+
+	log.Printf("Writing to socket")
+	_, err = dockerSock.Write([]byte("GET /images/json HTTP/1.1\n"))
+	_, err = dockerSock.Write([]byte("Host: http\n"))
+	_, err = dockerSock.Write([]byte("User-Agent: golang-client\n"))
+	_, err = dockerSock.Write([]byte("Accept: */*\n"))
+	_, err = dockerSock.Write([]byte("\n"))
+
+	if err != nil {
+		log.Fatal("write error:", err)
+	}
+
+	buf := make([]byte, 2064)
+	n, err := dockerSock.Read(buf[:])
+	if err != nil {
+		log.Fatal("read error:", err)
+	}
+
+	res := string(buf[0:n])
+	log.Printf("Docker daemon response: %v", res)
+
+	w.Write([]byte(res))
 }
